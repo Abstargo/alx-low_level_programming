39f715eadfc61dacdf357df6bc7bcf295c5bfea4
Yassir <james.iharo@gmail.com>
2023-06-17 23:16:59 +0100

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-/**
+#include<stdio.h>
+/** 
  * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    const char *message = "\"Programming is like building a multilingual puzzle";
-    puts(message);
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle
+");
+	return (0);
 }
