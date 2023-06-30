9a2b7218c188acd66787b0f90847563ad77146ee
Yassir <james.iharo@gmail.com>
2023-07-01 00:32:07 +0100

    Learning more about pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,23 @@
-#include "stdio.h"
-
+#include "main.h"
 /**
- * _strcmp - Compares two strings.
- * @s1: The first string.
- * @s2: The second string.
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
- * Return:
- *   s1[i] - s2[i]
+ * Return: s1[i] - s2[i]
  */
-int _strcmp(const char *s1, const char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
+	int i;
 
-	return *(unsigned char *)s1 - *(unsigned char *)s2;
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
