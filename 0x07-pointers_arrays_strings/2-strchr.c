30659597892141f37b742273fd1d5a25c51def31
Yassir <james.iharo@gmail.com>
2023-07-03 13:34:35 +0100

    Learning Even more about pointers and arrays also strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: Pointer to the string.
+ * @c: The character to be located.
+ *
+ * Return: Pointer to the first occurrence of the character @c in the string @s,
+ *         or 0 if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: Pointer to the string.
+ * @needle: Pointer to the substring.
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle) {
+    if (*needle == '\0') {
+        return haystack;
+    }
+
+    while (*haystack != '\0') {
+        char *h = haystack;
+        char *n = needle;
+
+        while (*n != '\0' && *h == *n) {
+            h++;
+            n++;
+        }
+
+        if (*n == '\0') {
+            return (haystack);
+        }
+
+        haystack++;
+    }
+
+    return (0);
+}
