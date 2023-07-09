5f553ca32e59302c272c8dfe3492bb6dd4651757
Yassir <james.iharo@gmail.com>
2023-07-09 12:05:19 +0100

    Learnin gabout argv and argc in c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
