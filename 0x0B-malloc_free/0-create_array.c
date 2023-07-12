e22b3048456aa1af1bfa23e227dac8f128f3762f
Yassir <james.iharo@gmail.com>
2023-07-12 14:59:21 +0100

    Learning about malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and initializes 
+ * create_array - creates an array of chars and initializes
  * it with a specific char
  * @size : The size of the array
  * @c: The character to ininitialize the array with
