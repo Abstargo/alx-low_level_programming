5c72487bc03d05ac10b0356062b4b8427dc96c44
Yassir <james.iharo@gmail.com>
2023-07-12 17:25:37 +0100

    I'm tired

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,22 +11,20 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int i = 0;
+	int is_number = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
+		{
 			sign *= -1;
+		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - '0');
-
-			if ((result > 0 && result > INT_MAX / 10) ||
-			    (result < 0 && result < INT_MIN / 10))
-			{
-				return ((sign > 0) ? INT_MAX : INT_MIN);
-			}
+			is_number = 1;
 		}
-		else if (result != 0)
+		else if (is_number)
 		{
 			break;
 		}
