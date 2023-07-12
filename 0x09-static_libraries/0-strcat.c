7f3f3435090c1487656fdbdef6db6c0c17a3bc4b
Yassir <james.iharo@gmail.com>
2023-07-12 17:41:49 +0100

    Bruh

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, const char *src)
+{
+	char *tbs = dest;
+
+	while (*tbs != '\0')
+	{
+		tbs++;
+	}
+
+	while (*src != '\0')
+	{
+		*tbs = *src;
+		tbs++;
+		src++;
+	}
+
+	*tbs = '\0';
+
+	return (dest);
+}
