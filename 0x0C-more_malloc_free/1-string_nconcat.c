c91c46f091b5460504d2819d3617cc00b2e9de00
Yassir <James.iharo@gmail.com>
2023-07-14 04:03:38 +0100

    it's 4 am

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,34 +20,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	/* Calculate the length of s1 */
 	while (s1[s1_len] != '\0')
 		s1_len++;
 
-	/* Calculate the length of s2 */
 	while (s2[s2_len] != '\0')
 		s2_len++;
 
-	/* Adjust n if it's greater than or equal to s2 length */
 	if (n >= s2_len)
 		n = s2_len;
 
-	/* Allocate memory for the concatenated string */
 	concatenated = malloc(sizeof(char) * (s1_len + n + 1));
 
 	if (concatenated == NULL)
-		return NULL;
+		return (NULL);
 
-	/* Copy s1 into concatenated */
 	for (i = 0; i < s1_len; i++)
 		concatenated[i] = s1[i];
 
-	/* Copy n bytes from s2 into concatenated */
 	for (j = 0; j < n; j++)
 		concatenated[i + j] = s2[j];
 
-	concatenated[i + j] = '\0'; /* Null-terminate the string */
+	concatenated[i + j] = '\0';
 
-	return concatenated;
+	return (concatenated);
 }
-
