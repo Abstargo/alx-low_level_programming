31ef165dfb3886628ac2d9e780099c17ed4e63ca
Yassir <james.iharo@gmail.com>
2023-07-14 02:56:04 +0100

    Learning C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The size of memory to be allocated
+ * 
+ * Return: Pointer to the allocated memory
+ * If malloc fails, the fuction terminates the process with
+ * a status value of 98
+*/
+
+void *malloc_checked(unsigned int b)
+{
+    void *ptr;
+    
+    ptr = malloc(b);
+
+    if (ptr == NULL)
+    {
+        exit(98);
+    }
+
+    return (ptr);
+}
