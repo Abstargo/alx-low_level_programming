dbf5a0b519246902322fbe081e988c33a84d7429
Yassir <James.iharo@gmail.com>
2023-07-17 14:45:47 +0100

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
 /**
  * ABS - Macro to compute the absolute value of a number
  * @x: The input number
@@ -6,3 +9,5 @@
  */
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif
