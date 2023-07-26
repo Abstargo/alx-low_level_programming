6a75d6b7b90f0ed9c5fd2bab0d1308cc4f3c35e8
Yassir <james.iharo@gmail.com>
2023-07-26 22:41:11 +0100

    lol

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "main.h"
 
 /**
  * is_positive_number - Checks if a string contains only positive digits.
@@ -17,4 +18,39 @@ int is_positive_number(const char *str)
         str++;
     }
     return (1);
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 on success, 98 on error.
+ */
+int main(int argc, char *argv[])
+{
+
+    char *num1;
+    char *num2;
+    int result;
+
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (98);
+    }
+
+    num1 = argv[1];
+    num2 = argv[2];
+
+    if (!is_positive_number(num1) || !is_positive_number(num2))
+    {
+        printf("Error\n");
+        return (98);
+    }
+
+    result = atoi(num1) * atoi(num2);
+    printf("%d\n", result);
+
+    return (0);
 }
