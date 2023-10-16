df84da5e71fde4e76fb0229a228098330fc17faa
Yassir <james.iharo@gmail.com>
2023-10-16 19:49:39 +0100

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, const char *src)
+{
+	char *tbs = dest;
+
+	while (*tbs != '\0')
+	{
+		tbs++;
+	}
+
+	while (*src != '\0')
+	{
+		*tbs = *src;
+		tbs++;
+		src++;
+	}
+
+	*tbs = '\0';
+
+	return (dest);
+}
