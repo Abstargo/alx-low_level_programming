6c5f67288181989367fcd80f305fda1d29427f6a
Yassir <james.iharo@gmail.com>
2023-10-21 00:35:26 +0100

    Dynamic librairy

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P /tmp 
+wget -P /tmp https://github.com/Abstargo/alx-low_level_programming/blob/e40fcf93fb8825a94e7dcba5115f37c11ab4209b/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
